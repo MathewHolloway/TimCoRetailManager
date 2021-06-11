8c010089ef293718340330765474c29962b44874
Mathew Holloway <17612802@students.southwales.ac.uk>
2021-06-11 16:28:14 +0100

    Add Product Display Functionality.
    Added products to DB and wired connections via the API to display products in the Sales Page.

--- TRMData/dbo/Tables/Product.sql
+++ TRMData/dbo/Tables/Product.sql
@@ -3,7 +3,9 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [ProductName] NVARCHAR(50) NOT NULL, 
     [Description] NVARCHAR(MAX) NOT NULL,
-    [RetailPrice] MONEY not null,
+    [RetailPrice] MONEY NOT NULL,
+    [QuantityInStock] INT NOT NULL DEFAULT 1,
     [CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
     [LastModified] DATETIME2 NOT NULL DEFAULT getutcdate()
+
 )
--- TRMDataManager/Controllers/UserController.cs
+++ TRMDataManager/Controllers/UserController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNet.Identity;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using TRMDataManager.Library.DataAccess;
